6ad8fcbd250012a5f0cff6d5595254bab945b2bc
machida <c0a231791c@edu.teu.ac.jp>
2024-10-01 15:16:49 +0900

    練習４こうかとん爆弾衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -46,6 +46,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾重なり
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         #if key_lst[pg.K_UP]:
